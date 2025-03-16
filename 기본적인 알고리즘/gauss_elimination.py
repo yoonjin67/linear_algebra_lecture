b0affb86bf32545bcc53a8aceb2306fd23adfea0
Yunjin Lee <gzblues61@gmail.com>
2025-03-16 15:03:43 +0900

    Added pandas in gauss elimination, planning to add into gauss jordan soon

--- 기본적인 알고리즘/gauss_elimination.py
+++ 기본적인 알고리즘/gauss_elimination.py
@@ -1,71 +1,84 @@
 import sys
 import numpy as np
+import pandas as pd
 
-def gauss_elim(matrix):
+def gauss_elim(matrixToEliminate):
     step = "1"
-    n = len(matrix)
-    for i in range(n):
-        if matrix[i][i] == 0.0:
+    rowsLength, columnsLength = matrixToEliminate.shape
+    for i in range(rowsLength):
+        if matrixToEliminate[i][i] == 0.0:
             print("가우스 소거법이 불가능합니다. (gauss elimination is impossible)")
+            print("대각성분이 0으로 보임.")
             print("행 교환으로 해결 가능한지 측정합니다.")
-            for k in range(i+1, n):
-                if matrix[k][i] != 0:
-                    matrix[i], matrix[k] = matrix[k], matrix[i]
+            for k in range(i+1, rowsLength):
+                if matrixToEliminate[k][i] != 0:
+                    matrixToEliminate.iloc[[i,k]] = matrixToEliminate.iloc[[k,i]].values
+                    #iloc에서 대괄호를 2번 써서 [[i,k]] 하면 i행과 k행을 선택
                     break
             else:
                 print("행 교환으로도 해결 불가능합니다.(tried all row interchange)")
-                return matrix, None  # Moved outside sys.exit()
+                return matrixToEliminate, None  # Moved outside sys.exit()
+            print("행 교환으로 해결 가능합니다. 가우스 소거법을 다시 진행합니다.")
 
-        for j in range(i+1, n): # j는 다음 줄들의 인덱스
-            ratio = matrix[j][i]/matrix[i][i]
-            #현재 줄의 대각선 부분을 분모로 두고 matrix[j][i]를 분자로 두고
-            for k in range(i,len(matrix[0])):  
-                matrix[j][k] -= ratio*matrix[i][k] #이렇게 빼 주면
-            #matrix[j][i]는 적어도 0이 된다.
+        for j in range(i+1, rowsLength): # j는 다음 줄들의 인덱스
+            ratio = matrixToEliminate.iloc[j, i]/matrixToEliminate.iloc[i, i]
+
+            #주의: df.iloc[i,k]와 같이 쓰면 [i][k] 위치의 요소를 선택.
+            #현재 줄의 대각선 부분을 분모로 두고 matrixToEliminate[j][i]를 분자로 두고
+
+            for k in range(i,columnsLength):  
+                matrixToEliminate.iloc[j,k] -= ratio*matrixToEliminate.iloc[i, k] #이렇게 빼 주면
+
+            #matrixToEliminate[j][i]는 적어도 0이 된다.
             #이렇게 자기 자신 아래 열의 같은 행 번호들을 0으로 만드는 과정을 그림으로 그리면
             #왼쪽 위 -> 오른쪽 아래로 그어진 대각선 하단은 모두 0이 되는 과정도 알 수 있어진다.
+
         print("elimination step "+step+":")
         step = chr(ord(step)+1)
-        print(matrix)
+        print(matrixToEliminate)
         print("-----------------")
 
 
-    print("----gauss eliminated matrix is:")
-    print(matrix)
+    print("----gauss eliminated matrixToEliminate is:")
+    print(matrixToEliminate)
 
-    x = np.zeros(n)  
-    for i in range(n-1, -1, -1):
-        x[i] = matrix[i][-1]
+    x = np.zeros(rowsLength)  
+    for i in range(rowsLength-1, -1, -1):
+        x[i] = matrixToEliminate.iloc[i, -1]
         # 현재 일차방정식의 상수항은 구하고자 하는 미지수만 포함하지 않음
-        for j in range(i+1, n):
-            x[i] -= matrix[i][j]*x[j]
+        for j in range(i+1, rowsLength):
+            x[i] -= matrixToEliminate.iloc[i, j]*x[j]
             # 상수항에서 이미 구한 미지수의 해*계수만큼을 곱해줌.
         # i*a = j
         # a = j/i
-        x[i] /= matrix[i][i]
+        x[i] /= matrixToEliminate.iloc[i, i]
     
-    return matrix, x
+    return matrixToEliminate, x
 
 # Test the function
-matrix = []
-n = int(input("연립일차방정식의 행을 숫자로 넣어 주세요. 연립일차방정식의 열은, 없는 항을 0으로 하여 숫자로 넣어 주세요.\n(상수항 포함한 첨가 행렬 꼴일 때)"))
+matrixToEliminate = []
+n = int(input("연립일차방정식의 계수행렬을 숫자로 넣어 주세요.\n"))
+print("열의 수는 행의 수보다 1만큼 많게 넣어주세요\n")
 for i in range(n):
     inp = input().split()
+    if len(inp) > n+1:
+        print("열의 수가 잘못되었습니니다.")
+        sys.exit(1)
     row = []
     for i in inp:
         row.append(int(i))
-    matrix.append(row)
+    matrixToEliminate.append(row)
 
-
-matrix = np.array(matrix, dtype=float)
-print("original matrix was:")
-print(matrix)
-a, b = gauss_elim(matrix)
+matrixToEliminate = np.array(matrixToEliminate,dtype=float)
+matrixToEliminate =  pd.DataFrame(matrixToEliminate)
+print("original matrixToEliminate was:")
+print(matrixToEliminate)
+a, b = gauss_elim(matrixToEliminate)
 
 if b is None:
     print("elimination failed!")
 else:
     val = 'a'
-    for i, value in enumerate(b):
-        print(f"{val} = {value}")
+    for i, row in matrixToEliminate.iterrows():
+        print(f"{val} = {row.iloc[-1]}")
         val = chr(ord(val) + 1)
